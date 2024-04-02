df50ba3b0f3f913b3736b5e3049e7acdcb3b16cd
Natalyyy18 <153389652+Natalyyy18@users.noreply.github.com>
2024-04-03 01:45:57 +0500

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,211 @@
+﻿using ClassLibrary10;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace _11_лабораторная_работа
+{
+    public class TestCollection
+    {
+        LinkedList<DebitCard> collection1 = new LinkedList<DebitCard>();
+        LinkedList<string> collection2 = new LinkedList<string>();  
+        SortedDictionary<BankCard, DebitCard> collection3 = new SortedDictionary<BankCard, DebitCard>();
+        SortedDictionary<string, DebitCard> collection4 = new SortedDictionary<string, DebitCard>();
+        
+        DebitCard first, middle, last;
+        DebitCard noexist = new DebitCard();
+        Stopwatch sw = new Stopwatch();
+
+        public TestCollection(int length) 
+        {
+            for (int i = 0; i < length; i++)
+            {
+                try
+                {
+                    DebitCard debit = new DebitCard();
+                    debit.RandomInit();
+                    debit.Name += i.ToString();
+                    collection1.AddLast(debit);
+                    collection2.AddLast(debit.ToString());
+
+                    collection3.Add(debit.GetBase, debit);//хз вторая штука я хз чем должна быть
+                    collection4.Add(debit.GetBase.ToString(), debit);
+                    if (i == 0)
+                        first = (DebitCard)debit.Clone();
+                    if (i == length / 2)
+                        middle = (DebitCard)debit.Clone();
+                    if (i == length - 1)
+                        last = (DebitCard)debit.Clone();
+                    
+                }
+                catch (Exception ex) 
+                {
+                    Console.WriteLine(ex); ;
+                }
+
+
+
+            }
+        }
+       
+        public void MeasureTime()
+        {
+            //foreach (var item in collection1)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            
+            Console.WriteLine("Поиск первого элемента");
+            sw.Restart();
+            bool isFindCol1 = collection1.Contains(first);
+            sw.Stop();
+            if(isFindCol1)
+                Console.WriteLine($"элемент найден за {sw.ElapsedTicks}");
+            else
+                Console.WriteLine($"элемент не найден за {sw.ElapsedTicks}");
+
+            sw.Restart();
+            bool isFindCol2 = collection2.Contains(first.ToString());
+            sw.Stop();
+            if (isFindCol2)
+                Console.WriteLine($"элемент найден за {sw.ElapsedTicks}");
+            else
+                Console.WriteLine($"элемент не найден за {sw.ElapsedTicks}");
+
+            sw.Restart();
+            bool isFindCol3 = collection3.ContainsKey(first.GetBase); //key
+            sw.Stop();
+            if (isFindCol3)
+                Console.WriteLine($"элемент по ключу найден за {sw.ElapsedTicks}");
+            else
+                Console.WriteLine($"элемент по ключу не найден за {sw.ElapsedTicks}");
+
+           
+
+            sw.Restart();
+            bool isFindCol4 = collection4.ContainsKey(first.GetBase.ToString()); //добавить с containsvalue
+            sw.Stop();
+            if (isFindCol4)
+                Console.WriteLine($"элемент найден за {sw.ElapsedTicks}");
+            else
+                Console.WriteLine($"элемент не найден за {sw.ElapsedTicks}");
+
+
+
+            Console.WriteLine("Поиск последнего элемента");
+            sw.Restart();
+            bool isFindLastCol1 = collection1.Contains(last);
+            sw.Stop();
+            if (isFindLastCol1)
+                Console.WriteLine($"элемент найден за {sw.ElapsedTicks}");
+            else
+                Console.WriteLine($"элемент не найден за {sw.ElapsedTicks}");
+
+            sw.Restart();
+            bool isFindLastCol2 = collection2.Contains(last.ToString());
+            sw.Stop();
+            if (isFindLastCol2)
+                Console.WriteLine($"элемент найден за {sw.ElapsedTicks}");
+            else
+                Console.WriteLine($"элемент не найден за {sw.ElapsedTicks}");
+
+            sw.Restart();
+            bool isFindLastCol3 = collection3.ContainsKey(last.GetBase); 
+            sw.Stop();
+            if (isFindLastCol3)
+                Console.WriteLine($"элемент по ключу найден за {sw.ElapsedTicks}");
+            else
+                Console.WriteLine($"элемент по ключу не найден за {sw.ElapsedTicks}");
+
+           
+
+            sw.Restart();
+            bool isFindLastCol4 = collection4.ContainsKey(last.GetBase.ToString()); //добавить с containsvalue
+            sw.Stop();
+            if (isFindLastCol4)
+                Console.WriteLine($"элемент найден за {sw.ElapsedTicks}");
+            else
+                Console.WriteLine($"элемент не найден за {sw.ElapsedTicks}");
+
+
+
+
+            Console.WriteLine("Поиск среднего элемента");
+            sw.Restart();
+            bool isFindLMiddleCol1 = collection1.Contains(middle);
+            sw.Stop();
+            if (isFindLMiddleCol1)
+                Console.WriteLine($"элемент найден за {sw.ElapsedTicks}");
+            else
+                Console.WriteLine($"элемент не найден за {sw.ElapsedTicks}");
+
+            sw.Restart();
+            bool isFindLMiddleCol2 = collection2.Contains(middle.ToString());
+            sw.Stop();
+            if (isFindLMiddleCol2)
+                Console.WriteLine($"элемент найден за {sw.ElapsedTicks}");
+            else
+                Console.WriteLine($"элемент не найден за {sw.ElapsedTicks}");
+
+            sw.Restart();
+            bool isFindLMiddleCol3 = collection3.ContainsKey(middle.GetBase); //key
+            sw.Stop();
+            if (isFindLMiddleCol3)
+                Console.WriteLine($"элемент по ключу найден за {sw.ElapsedTicks}");
+            else
+                Console.WriteLine($"элемент по ключу не найден за {sw.ElapsedTicks}");
+
+            
+
+            sw.Restart();
+            bool isFindLMiddleCol4 = collection4.ContainsKey(middle.GetBase.ToString()); //добавить с containsvalue
+            sw.Stop();
+            if (isFindLMiddleCol4)
+                Console.WriteLine($"элемент найден за {sw.ElapsedTicks}");
+            else
+                Console.WriteLine($"элемент не найден за {sw.ElapsedTicks}");
+
+
+
+            Console.WriteLine("Поиск несуществующего элемента");
+            sw.Restart();
+            bool isFindLNoExistCol1 = collection1.Contains(noexist);
+            sw.Stop();
+            if (isFindLNoExistCol1)
+                Console.WriteLine($"элемент найден за {sw.ElapsedTicks}");
+            else
+                Console.WriteLine($"элемент не найден за {sw.ElapsedTicks}");
+
+            sw.Restart();
+            bool isFindLNoExistCol2 = collection2.Contains(noexist.ToString());
+            sw.Stop();
+            if (isFindLNoExistCol2)
+                Console.WriteLine($"элемент найден за {sw.ElapsedTicks}");
+            else
+                Console.WriteLine($"элемент не найден за {sw.ElapsedTicks}");
+
+            sw.Restart();
+            bool isFindLNoExistCol3 = collection3.ContainsKey(noexist.GetBase); //key
+            sw.Stop();
+            if (isFindLNoExistCol3)
+                Console.WriteLine($"элемент по ключу найден за {sw.ElapsedTicks}");
+            else
+                Console.WriteLine($"элемент по ключу не найден за {sw.ElapsedTicks}");
+
+           
+
+            sw.Restart();
+            bool isFindLNoExistCol4 = collection4.ContainsKey(noexist.GetBase.ToString()); //добавить с containsvalue
+            sw.Stop();
+            if (isFindLNoExistCol4)
+                Console.WriteLine($"элемент найден за {sw.ElapsedTicks}");
+            else
+                Console.WriteLine($"элемент не найден за {sw.ElapsedTicks}");
+
+           
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,211 @@
+﻿using ClassLibrary10;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _11_лабораторная_работа
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Dictionary
+            //dictionary
+
+            Dictionary<BankCard, DebitCard> dict = new Dictionary<BankCard, DebitCard>();
+            Console.WriteLine($"в словаре {dict.Count} элементов");
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    DebitCard s = new DebitCard();
+                    s.RandomInit();
+                    var milliseconds = 300;
+                    Thread.Sleep(milliseconds);
+                    BankCard b = new BankCard(s.Number, s.Name, s.Term, s.id.number1);
+                    dict.Add(b, s);
+                }
+                catch (Exception e)
+                {
+                    i--;
+                }
+            }
+            Console.WriteLine("Dictionary:");
+            foreach (var item in dict.Keys)
+            {
+                Console.WriteLine($"ключ: {item}, значение: {dict[item]}");
+            }
+            Console.WriteLine($"в словаре {dict.Count} элементов");
+            //Console.WriteLine("vvedite element for search");
+
+
+
+            Console.WriteLine("vvedite element for search");
+            DebitCard find = new DebitCard();
+            find.Init();
+
+            BankCard b1 = new BankCard(find.Number, find.Name, find.Term, find.id.number1);
+            dict.Add(b1, find);
+
+            Console.WriteLine("Dictionary with new element:");
+            foreach (var item in dict.Keys)
+            {
+                Console.WriteLine($"ключ: {item}, значение: {dict[item]}");
+            }
+
+            Console.WriteLine($" после добавления в словаре {dict.Count} элементов");
+
+
+
+            Console.WriteLine("");
+            if (dict.ContainsKey(b1))
+                Console.WriteLine($"Найден по ключу"); //добавить ключ
+            else
+                Console.WriteLine("Не найден");
+
+
+            Console.WriteLine("");
+            if (dict.ContainsValue(find))
+                Console.WriteLine($"Найден по значению"); //добавить значению
+            else
+                Console.WriteLine("Не найден");
+
+            dict.Remove(b1);
+            Console.WriteLine($" После удаления в словаре {dict.Count} элементов");
+            Console.WriteLine($" Словарь после удаления:");
+            foreach (var item in dict.Keys)
+            {
+                Console.WriteLine($"ключ: {item}, значение: {dict[item]}");
+            }
+
+
+            Dictionary<BankCard, DebitCard> dictclone = new Dictionary<BankCard, DebitCard>();
+            foreach (var tt in dict)
+            {
+                DebitCard dictclonecard = tt.Value.Clone() as DebitCard;
+                dictclone.Add(tt.Key, dictclonecard);
+            }
+            Console.WriteLine("КЛОН СЛОВАРЯ:");
+            foreach (var item in dictclone.Keys)
+            {
+                Console.WriteLine($"ключ: {item}, значение: {dictclone[item]}");
+            }
+
+
+
+
+
+            #region ArrayList
+            //ArrayList
+
+            ArrayList list1 = new ArrayList();
+            for (int i = 0; i < 5; i++)
+            {
+                BankCard c = new BankCard();
+                list1.Add(c);
+                c.RandomInit();
+                var milliseconds = 300;
+                Thread.Sleep(milliseconds);
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                CreditCard r = new CreditCard();
+                list1.Add(r);
+                r.RandomInit();
+                var milliseconds = 300;
+                Thread.Sleep(milliseconds);
+            }
+            Console.WriteLine($" Лист для 1 задания:");
+            list1.Sort();
+            foreach (object c in list1)
+            {
+                Console.WriteLine(c.ToString());
+            }
+
+
+
+            Console.WriteLine($"Емкость = {list1.Capacity}");
+            Console.WriteLine($"Количество элем = {list1.Count}");
+
+
+            Console.WriteLine("Общий баланс дебетовых карт:"); //считаем вместе с молодежными
+            double sum = 0;
+            double count = 0;
+            foreach (BankCard item in list1)
+            {
+                if (item is DebitCard t)
+                {
+                    sum += t.GetBalance();
+                    count++;
+                }
+            }
+            Console.WriteLine($"{sum} рублей");
+
+            Console.WriteLine("");
+            Console.WriteLine("Имена людей с истекшим сроком карты:");
+            DateTime m = DateTime.UtcNow;
+            foreach (BankCard item in list1)
+            {
+                if (item is BankCard s)
+                    if (s.Term <= DateTime.Today)
+                        Console.WriteLine(item.Name);
+            }
+            //search and remove
+            Console.WriteLine("Введите элемент для поиска:");
+            BankCard bankc = new BankCard();
+            bankc.Init();
+            list1[2] = bankc;
+            list1.Sort();
+            foreach (BankCard item in list1)
+            { Console.WriteLine(item); }
+            Console.WriteLine("");
+            int pos = list1.IndexOf(bankc);
+            if (pos >= 0)
+            {
+                Console.WriteLine($"Удалено: {list1[pos]}");
+                list1.RemoveAt(pos);
+            }
+            if (list1.Contains(bankc))
+            {
+                Console.WriteLine("Найден");
+            }
+            else
+                Console.WriteLine("Не найден");
+
+            ArrayList sec_list = new ArrayList();
+            sec_list = (ArrayList)list1.Clone();
+            Console.WriteLine("\nElements of Cloned ArrayList: \n");
+            foreach (object str1 in sec_list)
+            {
+                Console.WriteLine(str1);
+            }
+            BankCard card = new BankCard();
+            card.RandomInit();
+            BankCard card1 = new BankCard();
+            card1.RandomInit();
+
+            //YoungCard young = new YoungCard();
+            //young.RandomInit();
+
+            //DebitCard debit = new DebitCard();
+            //debit.RandomInit();
+
+            //CreditCard credit = new CreditCard();
+            //credit.RandomInit();
+            Console.WriteLine("");
+            Console.WriteLine("Измерение времени коллекций:");
+            TestCollection collection = new TestCollection(1000);
+            collection.MeasureTime();
+
+
+            #endregion
+            #endregion
+
+        }
+    }
+}
